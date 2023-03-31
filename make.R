9bb7580250916d2e64933bfc3c6b7f3ee9daae97
see24 <sarahendicott4@gmail.com>
2023-03-31 09:17:10 -0400

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' cloudDemo: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sarah Endicott \email{sarah.endicott@canada.ca}
+#' 
+#' @date 2023/03/31
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+renv::restore()
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
